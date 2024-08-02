3c56626f35bf5ffbafc6014c9c17531e5afdbc7d
jvc-byte <vernondirk419@gmail.com>
2024-08-02 18:49:18 +0100

    use code.length

--- contracts/bank.sol
+++ contracts/bank.sol
@@ -2,67 +2,57 @@
 pragma solidity ^0.8.24;
 
 contract bank {
-    mapping (address => uint64) public record;
+    mapping(address => uint64) public record;
 
     // uint64 public MinDepAmt = 250000000000000 wei;
     // uint64 public MaxWithdAmt = 2 ether;
 
-    modifier CheckMinDepAmt(uint64 amt)
-    {
-        require(amt >= 250000000000000/*MinDepAmt*/, "Minimun Deposit amount is 0.25 Ether");
+    modifier CheckMinDepAmt(uint64 amt) {
+        require(
+            amt >= 250000000000000, /*MinDepAmt*/
+            "Minimun Deposit amount is 0.25 Ether"
+        );
         _;
     }
 
-    modifier CheckMaxWithdAmt(uint64 amt)
-    {
-        require(amt <= 2000000000000000000 /*MaxWithdAmt*/, "Max Withdrawal Amount is 2 Ether");
+    modifier CheckMaxWithdAmt(uint64 amt) {
+        require(
+            amt <= 2000000000000000000, /*MaxWithdAmt*/
+            "Max Withdrawal Amount is 2 Ether"
+        );
         _;
     }
 
-    modifier VerifyAddr(address addr)
-    {
+    modifier VerifyAddr(address addr) {
         require(!ContractAddr(addr), "This is a contract address!");
         _;
     }
 
     // set a minimum amount for deposit
-    function deposit(address DepAddr, uint64 DepAmt) 
-    public
-    VerifyAddr(DepAddr)// ensure the caller is not a contract address
-    CheckMinDepAmt(DepAmt) 
+    function deposit(address DepAddr, uint64 DepAmt)
+        public
+        VerifyAddr(DepAddr) // ensure the caller is not a contract address
+        CheckMinDepAmt(DepAmt)
     {
         record[DepAddr] += DepAmt;
     }
 
     // set a maximum amount for withdrawal (withdrawal limit)
     function withdraw(address WithdAddr, uint64 WithdAmt)
-    public
-    VerifyAddr(WithdAddr)
-    CheckMaxWithdAmt(WithdAmt)
+        public
+        VerifyAddr(WithdAddr)
+        CheckMaxWithdAmt(WithdAmt)
     {
         record[WithdAddr] -= WithdAmt;
     }
 
     // Helper function to check for contract address
-    function ContractAddr(address _addr)
-    internal 
-    view 
-    returns (bool)
-    {
-        uint64 BytecodeSize;
-        assembly
-        {
-            BytecodeSize := extcodesize(_addr)
-        }
-        return BytecodeSize > 0;
+    function ContractAddr(address _addr) internal view returns (bool) {
+        return _addr.code.length > 0;
     }
-    
+
     // Check user ballance
-    function GetBalance(address UserAddr)
-    public
-    view 
-    returns (uint64)
-    {
+    function GetBalance(address UserAddr) public view returns (uint64) {
         return record[UserAddr];
     }
 }
