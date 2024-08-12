078745a5f16d5c553959f1773056ddc0a1b96e4f
jvc-byte <vernondirk419@gmail.com>
2024-08-12 23:26:19 +0100

    added IERC20 for token

---
+++ contracts/bank.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract bank {
+    mapping(address => uint64) public record;
+    mapping(string => User) private users;
+
+    uint64 private MinDepAmt = 250000000000000 wei;
+    uint64 private MaxWithdAmt = 2 ether;
+
+    struct User {
+        string uname;
+        address uaddress;
+        uint64 balance;
+    }
+
+    enum Status {
+        Pending,
+        Failed,
+        Successful
+    }
+    Status public status;
+    event DepositWithdrawError(string message);
+
+    modifier CheckMinDepAmt(uint64 amt) {
+        if (amt > MinDepAmt) {
+            status = Status.Successful;
+            emit DepositWithdrawError("Deposit Succesfull");
+        } else {
+            status = Status.Failed;
+            emit DepositWithdrawError("Deposit Failed");
+        }
+        // require(amt > MinDepAmt, "Minimun Deposit amount is 0.25 Ether");
+        _;
+    }
+
+    modifier CheckMaxWithdAmt(uint64 amt) {
+        if (amt < MaxWithdAmt) {
+            status = Status.Successful;
+            emit DepositWithdrawError("Withdrawal Successful");
+        } else {
+            status = Status.Failed;
+            emit DepositWithdrawError("Withdrawal Failed");
+
+        }
+        // require(amt <= MaxWithdAmt, "Max Withdrawal Amount is 2 Ether");
+        _;
+    }
+
+    modifier VerifyAddr(address addr) {
+        require(!ContractAddr(addr), "This is a contract address!");
+        _;
+    }
+    // Check if user exist in the system
+    modifier VerifyUname(string memory uname) {
+        require(users[uname].uaddress != address(0), "User not Found!");
+        _;
+    }
+
+    // Function to register a user
+    function registerUser(
+        string memory uname,
+        address uaddress,
+        uint64 balance
+    ) public {
+        users[uname] = User(uname, uaddress, balance);
+    }
+
+    // Function to get a user by username
+    function getUserByUsername(string memory uname)
+        public
+        view
+        VerifyUname(uname)
+        returns (User memory)
+    {
+        return users[uname];
+    }
+
+    // set a minimum amount for deposit
+    function deposit(address DepAddr, uint64 DepAmt)
+        public
+        VerifyAddr(DepAddr) // ensure the caller is not a contract address
+        CheckMinDepAmt(DepAmt)
+    {
+        record[DepAddr] += DepAmt;
+    }
+
+    // set a maximum amount for withdrawal (withdrawal limit)
+    function withdraw(address WithdAddr, uint64 WithdAmt)
+        public
+        VerifyAddr(WithdAddr) // ensure the caller is not a contract address
+        CheckMaxWithdAmt(WithdAmt)
+    {
+        record[WithdAddr] -= WithdAmt;
+    }
+
+    // Helper function to check for contract address
+    function ContractAddr(address _addr) internal view returns (bool) {
+        return _addr.code.length > 0;
+    }
+}
