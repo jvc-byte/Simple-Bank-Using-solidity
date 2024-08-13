5f1656051b5e62f0ce3151d966b2d5e2106adbd3
jvc-byte <vernondirk419@gmail.com>
2024-08-14 00:45:15 +0100

    add .deps and mint new token

--- contracts/token.sol
+++ contracts/token.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract MyToken is ERC20 {
     constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
-        ERC20Permit(name)  // ERC20Permit requires the token's name
     {
         // Mint 100 tokens to msg.sender with default 18 decimals
         _mint(msg.sender, 100 * 10 ** uint256(decimals()));
